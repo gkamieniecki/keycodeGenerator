fead15a84a69c71618332bb31e785fd9a11dd32a
gkamieniecki <grz.kamieniecki@gmail.com>
2021-04-19 21:06:23 +0100

    fixed event.which return value and added minor design changes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ document.addEventListener("keydown", () => {
     document.getElementById("keycode-display").innerHTML = event.key;
     document.getElementById("key-value").innerHTML = event.key;
     document.getElementById("code-value").innerHTML = event.keyCode;
-    document.getElementById("which-value").innerHTML = event.composed;
+    document.getElementById("which-value").innerHTML = event.code;
 
 })
